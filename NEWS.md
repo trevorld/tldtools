2de7cbed5e6da0caf16f4219248ef7eaa6a44472
Trevor L Davis <trevor.l.davis@gmail.com>
2025-10-08 12:05:58 -0700

    feat: `check_air()`, `use_tld_air()`, `use_tld_Rbuildignore()`
    closes #8, closes #9

--- NEWS.md
+++ NEWS.md
@@ -4,7 +4,10 @@ tldtools 0.0.1 (development)
 New features
 ------------
 
+* `check_air()` (#8)
 * `check_github_labels()` (#4)
 * `check_github_settings()` (#1)
+* `use_tld_air()` (#8)
 * `use_tld_github_labels()` (#2)
 * `use_tld_github_settings()` (#3)
+* `use_tld_Rbuildignore()` (#9)
--- R/check_github_labels.r
+++ R/check_github_labels.r
@@ -27,55 +27,68 @@ class(tld_github_labels$color) <- "gh_color"
 #' @seealso [use_tld_github_labels()]
 #' @export
 check_github_labels <- function() {
+	message <- character(0L)
 
-    message <- character(0L)
+	df <- ghcli::gh_label_list()
 
-    df <- ghcli::gh_label_list()
+	# labels in `df` but not in `tld_github_labels`
+	only_in_df <- setdiff(df$name, tld_github_labels$name)
+	if (length(only_in_df)) {
+		message <- c(
+			message,
+			"x" = paste("GitHub has extra labels:", paste(only_in_df, collapse = ", "))
+		)
+	} else {
+		message <- c(message, "v" = "GitHub does not have extra labels")
+	}
 
-    # labels in `df` but not in `tld_github_labels`
-    only_in_df <- setdiff(df$name, tld_github_labels$name)
-    if (length(only_in_df))
-        message <- c(message,
-                     "x" = paste("GitHub has extra labels:",
-                                 paste(only_in_df, collapse = ", ")))
-    else
-        message <- c(message, "v" = "GitHub does not have extra labels")
+	# labels in `tld_github_labels` but not in `df`
+	only_in_tld <- setdiff(tld_github_labels$name, df$name)
+	if (length(only_in_tld)) {
+		message <- c(
+			message,
+			"x" = paste("GitHub is missing labels: ", paste(only_in_tld, collapse = ", "))
+		)
+	} else {
+		message <- c(message, "v" = "GitHub is not missing labels")
+	}
 
-    # labels in `tld_github_labels` but not in `df`
-    only_in_tld <- setdiff(tld_github_labels$name, df$name)
-    if (length(only_in_tld))
-        message <- c(message,
-                     "x" = paste("GitHub is missing labels: ",
-                                 paste(only_in_tld, collapse = ", ")))
-    else
-        message <- c(message, "v" = "GitHub is not missing labels")
+	# labels in both but different descriptions
+	in_both <- intersect(df$name, tld_github_labels$name)
+	df_b <- df[which(df$name %in% in_both), ] |>
+		dplyr::arrange(.data$name)
+	tld_b <- tld_github_labels[which(tld_github_labels$name %in% in_both), ] |>
+		dplyr::arrange(.data$name)
 
-    # labels in both but different descriptions
-    in_both <- intersect(df$name, tld_github_labels$name)
-    df_b <- df[which(df$name %in% in_both), ] |>
-        dplyr::arrange(.data$name)
-    tld_b <- tld_github_labels[which(tld_github_labels$name %in% in_both), ] |>
-        dplyr::arrange(.data$name)
+	descriptions_different <- which(df_b$description != tld_b$description)
+	if (length(descriptions_different)) {
+		message <- c(
+			message,
+			"x" = paste(
+				"Different label descriptions: ",
+				paste(df_b$name[descriptions_different], collapse = ", ")
+			)
+		)
+	} else {
+		message <- c(message, "v" = "Label descriptions are as expected")
+	}
 
-    descriptions_different <- which(df_b$description != tld_b$description)
-    if (length(descriptions_different))
-        message <- c(message,
-                     "x" = paste("Different label descriptions: ",
-                                 paste(df_b$name[descriptions_different], collapse = ", ")))
-    else
-        message <- c(message, "v" = "Label descriptions are as expected")
+	# labels in both but different color
+	colors_different <- which(tolower(df_b$color) != tolower(tld_b$color))
+	if (length(colors_different)) {
+		message <- c(
+			message,
+			"x" = paste(
+				"Different label colors: ",
+				paste(df_b$name[colors_different], collapse = ", ")
+			)
+		)
+	} else {
+		message <- c(message, "v" = "Label colors are as expected")
+	}
 
-    # labels in both but different color
-    colors_different <- which(tolower(df_b$color) != tolower(tld_b$color))
-    if (length(colors_different))
-        message <- c(message,
-                     "x" = paste("Different label colors: ",
-                                 paste(df_b$name[colors_different], collapse = ", ")))
-    else
-        message <- c(message, "v" = "Label colors are as expected")
-
-    cli_inform(message, class = "tldtools_check_github_settings")
-    invisible(NULL)
+	cli_inform(message, class = "tldtools_check_github_settings")
+	invisible(NULL)
 }
 
 #' Use Trevor's preferred GitHub labels
@@ -90,21 +103,21 @@ check_github_labels <- function() {
 #' @seealso [check_github_labels()]
 #' @export
 use_tld_github_labels <- function() {
-    df <- ghcli::gh_label_list()
+	df <- ghcli::gh_label_list()
 
-    to_edit <- intersect(tld_github_labels$to_edit, df$name)
-    df_to_edit <- tld_github_labels[which(tld_github_labels$to_edit %in% to_edit), ]
-    l <- purrr::pmap(df_to_edit, ghcli::gh_label_edit)
+	to_edit <- intersect(tld_github_labels$to_edit, df$name)
+	df_to_edit <- tld_github_labels[which(tld_github_labels$to_edit %in% to_edit), ]
+	l <- purrr::pmap(df_to_edit, ghcli::gh_label_edit)
 
-    to_edit2 <- intersect(tld_github_labels$name, df$name)
-    df_to_edit2 <- tld_github_labels[which(tld_github_labels$name %in% to_edit2), 1:3] |>
-        dplyr::rename(to_edit = name)
-    l <- purrr::pmap(df_to_edit2, ghcli::gh_label_edit)
+	to_edit2 <- intersect(tld_github_labels$name, df$name)
+	df_to_edit2 <- tld_github_labels[which(tld_github_labels$name %in% to_edit2), 1:3] |>
+		dplyr::rename(to_edit = .data$name)
+	l <- purrr::pmap(df_to_edit2, ghcli::gh_label_edit)
 
-    df <- ghcli::gh_label_list()
-    to_add <- setdiff(tld_github_labels$name, df$name)
-    df_to_add <- tld_github_labels[which(tld_github_labels$name %in% to_add), 1:3]
-    l <- purrr::pmap(df_to_add, ghcli::gh_label_create)
+	df <- ghcli::gh_label_list()
+	to_add <- setdiff(tld_github_labels$name, df$name)
+	df_to_add <- tld_github_labels[which(tld_github_labels$name %in% to_add), 1:3]
+	l <- purrr::pmap(df_to_add, ghcli::gh_label_create)
 
-    invisible(NULL)
+	invisible(NULL)
 }
